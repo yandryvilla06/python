6f26480ef98f9703c235574115026acf78036b7d
yandry <yandry75@gmail.com>
2021-07-07 18:48:43 +0200

    hola

---
+++ variables/variable.py
@@ -0,0 +1,16 @@
+numero=2 
+numero="pepe" #con esto machacamos , ya que le asignamos un valor a estas variables
+print(numero)
+
+#Concatenacion
+
+nombre="Yandry"
+apellido="villa"
+
+completo=nombre+" " + apellido #concatenamos con el +
+print(completo)
+
+print(f"{nombre} {apellido}") #concatenamos con la f
+
+print("hola mi nombre es {} y mi apellido es ".format(nombre,apellido)) 
+#concatenamos segun con llaves las palabras que queramos incluir 
