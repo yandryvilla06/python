74198316a611df2aadca39f4f9dc6a7d0bbd8075
yandry <yandry75@gmail.com>
2021-07-07 19:05:41 +0200

    tipos de datos

---
+++ variables/tipo.py
@@ -0,0 +1,18 @@
+nada=None
+print(nada)
+
+#imprimir el tipo de dato usamos type, me dice si es un string,int
+
+print(type(nada))
+
+
+cadena="yandry"
+
+print(type(cadena))
+
+lista=[1,2,3,4]
+tupla=("master", "en","PYTHON") #lista de datos que no pueden cambias
+diccionario={"nombre":"yandry","apellido":"Yandry"}#lista de datos con clave y valor
+print(lista)
+print(tupla)
+print(diccionario)
